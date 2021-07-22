15a6d379c097ecf1e07e1411f062f4f8e4c3d8ce
DavidYu1010 <provic0099@gmail.com>
2021-07-22 12:03:31 -0700

    add text field and button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <TextField id="standard-basic" label="Plain Text" />
+        <Button variant="contained">Encrypt</Button>
+      </div>
+      <div>
+      <TextField id="standard-basic" label="Cipher Text" />
+        <Button variant="contained">Decrypt</Button>
+      </div>
     </div>
   );
 }
