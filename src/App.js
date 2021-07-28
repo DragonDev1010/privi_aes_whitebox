41077e3f30a49b81b15bc26fd3251df456974f16
DavidYu1010 <provic0099@gmail.com>
2021-07-28 02:20:43 -0700

    fix decrypt issue

--- src/App.js
+++ src/App.js
@@ -6,31 +6,24 @@ var aes = require('./aes/tmp/wbaes-smoke-umd.js')
 
 
 function App() {
-  var plaintext = 'hello world'
+  var plaintext = 'hello'
   var options = {
       counter: '1826e4111826e4111826e4111826e411', 
       encoding: 'str'
   };
   var ciphertext = aes.encrypt(plaintext, options);
   var output = aes.decrypt(ciphertext, options)
-  console.log('primary')
-  console.log(ciphertext, ' : ', output)
 
   var [plain, setPlain] = useState('')
   var [cipher, setCipher] = useState('')
-
+  var [decryptResult, setDecryptResult] = useState('')
   function encrypt() {
     var tmp = aes.encrypt(plain, options);
-    var v = aes.decrypt(tmp, options)
-    console.log('cipher: ', tmp)
-    console.log('plain: ', v)
     setCipher(tmp)
   }
   function decrypt() {
-    // console.log('cipher: ', cipher)
-    // var tmp_plain = aes.decrypt(cipher, options)
-    // console.log('plain temp: ', tmp_plain)
-    // setPlain(tmp_plain)
+    var tmp_plain = aes.decrypt(cipher, options)
+    setDecryptResult(tmp_plain)
   }
   return (
     <div className="App">
@@ -40,14 +33,14 @@ function App() {
         <label>cipher text: </label><p>{ciphertext}</p>
       </div>
       <div>
-        <input value={plain} onInput={e=>setPlain(e.target.event)}></input>
+        <input type = "text" value={plain} onChange={e=>setPlain(e.target.value)}></input>
         <button onClick={encrypt}>encrypt</button>
         <input value={cipher}></input>
       </div>
       <div>
         <input value={cipher}></input>
         <button onClick={decrypt}>Decrypt</button>
-        <input value={plain}></input>
+        <input value={decryptResult}></input>
       </div>
     </div>
   );
